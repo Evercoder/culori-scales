ce45666165d20bd9ee96d9deb87e3f3be39044a3
Dan Burzo <danburzo@gmail.com>
2018-04-08 19:00:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import scalesColorbrewer from './scales/colorbrewer';
+import scalesMpl from './scales/mpl';
+import scalesPerceptualRainbow from './scales/perceptualRainbow';
+
+import schemesColorbrewer from './schemes/colorbrewer';
+
+export default {
+	scalesColorbrewer,
+	scalesMpl,
+	scalesPerceptualRainbow,
+	schemesColorbrewer
+};
